f20a7cd11abe2321b71f54dc2fb8f193f2c57ec1
Antoine LOUBIERES <anloubie@e1r7p16.42.fr>
2020-01-28 17:09:44 +0100

    fixed echo

--- iscmd.c
+++ iscmd.c
@@ -6,7 +6,7 @@
 /*   By: anloubie <anloubie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 16:08:15 by anloubie          #+#    #+#             */
-/*   Updated: 2020/01/28 16:59:24 by anloubie         ###   ########.fr       */
+/*   Updated: 2020/01/28 17:09:12 by anloubie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void        is_command(char *cmd)
 {
     if (!ft_strncmp(cmd, "exit", 5))
 		exit (1);
-    if (!(ft_strncmp(cmd, "echo", 4)))
+    if (!(ft_strncmp(cmd, "echo ", 5)) || !ft_strncmp(cmd, "echo", 5))
         ft_echo(cmd);
     else
         ft_not_found(cmd);
