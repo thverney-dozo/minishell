cf04086fc8b56eba83849587fc1c6406e2618a78
Theo VERNEY <thverney@e1r6p14.42.fr>
2020-01-29 20:42:14 +0100

    multi echo

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 16:41:06 by anloubie          #+#    #+#             */
-/*   Updated: 2020/01/29 14:56:33 by thverney         ###   ########.fr       */
+/*   Updated: 2020/01/29 20:40:58 by thverney         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,13 @@
 void		ft_echo(char *cmd)
 {
 	char	*new;
+	int		i;
 
-	new = ft_substr(cmd, 5, ft_strlen(cmd));
+	i = 0;
+	while (cmd[i] != '\0' && cmd[i] != ';')
+		i++;
+	i -= 5;
+	new = ft_substr(cmd, 5, i);
 	write(1, new, ft_strlen(new));
 	write(1, "\n", 1);
 }
