8aa6f3339786edd0ead3d033d26d2f9406ab3820
Theo VERNEY <thverney@e1r9p15.42.fr>
2020-01-29 11:32:22 +0100

    yes

--- ft_not_found.c
+++ ft_not_found.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_not_found.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anloubie <anloubie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 16:58:31 by anloubie          #+#    #+#             */
-/*   Updated: 2020/01/28 17:00:04 by anloubie         ###   ########.fr       */
+/*   Updated: 2020/01/29 11:31:52 by thverney         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 void		ft_not_found(char *cmd)
 {
+	int i;
+
+	i = 0;
 	write(1, "minishell: command not found: ", 30);
+	while (cmd[i] < 33)
+		i++;
+	while (cmd[i] > 32)
+		i++;
+	cmd[i] = '\0';
 	write(1, cmd, ft_strlen(cmd));
 	write(1, "\n", 1);
 }
