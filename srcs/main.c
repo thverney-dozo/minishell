94ca43e516a65fba1b6e31b6c8904399215f10a3
Theo VERNEY <thverney@e1r6p14.42.fr>
2020-01-29 20:18:20 +0100

    ; working

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 18:43:30 by thverney          #+#    #+#             */
-/*   Updated: 2020/01/29 14:56:49 by thverney         ###   ########.fr       */
+/*   Updated: 2020/01/29 20:15:11 by thverney         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,28 @@ void	loop_shell(t_env *env)
 {
 	int		ret;
 	char	*line;
-
+	
 	(void)env;
 	while ((ret = get_next_line(0, &line)) > 0)
 	{
-		is_command(line);
-		if (line[0] == '\0')
+		while (*line < 33 && *line != '\0')
+			line++;
+		if (line[0] == '\0' || (line[0] == ';' && line[1] == '\0'))
 			return ;
-		if (line[ft_strlen(line)] == '\0')
-			break ;
+		env->pos_prev = line;
+		is_command(env->pos_prev);
+		env->pos_next = env->pos_prev;
+		while ((env->pos_prev = ft_strchr(env->pos_next, ';')))
+		{
+			env->pos_prev++;
+			while (*env->pos_prev < 33 && *env->pos_prev != '\0')
+				env->pos_prev++;
+			if (env->pos_prev[0] == '\0')
+				return ;
+			env->pos_next = ft_strchr(env->pos_prev, ';');
+			is_command(env->pos_prev);
+		}
+		break ;
 	}
 }
 
