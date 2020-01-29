704c86b67d57f8a904344ce71987540d7e3caa69
Antoine LOUBIERES <anloubie@e1r9p14.42.fr>
2020-01-29 12:04:27 +0100

    else if

--- iscmd.c
+++ iscmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iscmd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anloubie <anloubie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 16:08:15 by anloubie          #+#    #+#             */
-/*   Updated: 2020/01/29 11:26:57 by thverney         ###   ########.fr       */
+/*   Updated: 2020/01/29 11:28:10 by anloubie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void        is_command(char *cmd)
 {
     if (!ft_strncmp(cmd, "exit", 5))
 		exit (1);
-    if (!(ft_strncmp(cmd, "echo ", 5)) || !ft_strncmp(cmd, "echo", 5))
+    else if (!(ft_strncmp(cmd, "echo ", 5)) || !ft_strncmp(cmd, "echo", 5))
         ft_echo(cmd);
     else if (cmd[0] != '\0')
         ft_not_found(cmd);
