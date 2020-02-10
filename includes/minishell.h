ca348010d17dda332278d955cfcc8f9159771dec
Antoine LOUBIERES <anloubie@e1r13p11.42.fr>
2020-02-10 17:20:44 +0100

    added proto .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anloubie <anloubie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 18:49:54 by thverney          #+#    #+#             */
-/*   Updated: 2020/02/10 14:07:09 by thverney         ###   ########.fr       */
+/*   Updated: 2020/02/10 17:20:10 by anloubie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,62 +43,63 @@ typedef struct		s_cmd
 
 typedef struct		s_env
 {
-	int			max;
-	int			count;
-	int			fd[2];
-	int			old_fd;
-	int			input_pipe;
-	int			exit;
-	char		*buf;
-	char		*arg;
-	char		**my_env;
-	char		*dir;
-	char		**args;
-	char		**flags;
-	int			i;
-	int			j;
-	int			x;
-	int			y;
-	char		**copy_args;
-	char		*copy_free;
-	char		**av_pipe;
-	char		**copy_pipe;
-	char		**path;	
-	t_var		*var;
-	t_var		*first;
-	t_var		*save;
+	int				max;
+	int				count;
+	int				fd[2];
+	int				old_fd;
+	int				input_pipe;
+	int				exit;
+	char			*buf;
+	char			*arg;
+	char			**my_env;
+	char			*dir;
+	char			**args;
+	char			**flags;
+	int				i;
+	int				j;
+	int				x;
+	int				y;
+	char			**copy_args;
+	char			*copy_free;
+	char			**av_pipe;
+	char			**copy_pipe;
+	char			**path;	
+	t_var			*var;
+	t_var			*first;
+	t_var			*save;
 	
-}				t_env;
+}					t_env;
 
-void			loop_shell(t_env *env);
-void			is_command(char *cmd, t_env *env);
-void			ft_echo(t_env *env);
-void			ft_echo_n(int i, int tmp, t_env *env);
-void			ft_echo_two(t_env *env);
-void			ft_not_found(char *cmd);
-void			ft_pwd(void);
-void			ft_cd(t_env *env);
-void			ft_env(t_env *env);
-void			is_pipe_here(t_env *env);
-void			ft_pipe_is_cmd(t_env *env, int old_fd);
-void			verify_cmd_pipe(char *cmd, int indic, t_env *env);
-void			ft_get_dir(t_env *env);
-void			ft_export(char *str, t_env *env);
-t_var			*ft_lstvar(t_env *env);
-void			ft_save(t_env *env, t_var *var);
-void			ft_lstaddvar2(t_var *new, t_env *env);
-void			ft_lstaddvar(t_var **alst, t_var *new);
-t_var			*ft_lstnewvar(char *var, int i, t_env *env);
-int				ft_find_char(char *str, char c);
-int				ft_verif_var(char *name, t_env *env, char *value);
-void			ft_clear(void);
-void			ft_unset(char *str, t_env *env);
-void			ft_unset_var(char *str, t_env *env);
-char			*ft_get_home(t_env *env);
-int				is_executable(t_env *env, int indic);
-char			**split_commands(t_env *env);
-int				is_multi_line_quote(t_cmd *cmd, int i);
-void			is_multi_line_quote_two(t_cmd *cmd, int i);
-char			*get_semi_coma(t_cmd *cmd, char *str, char c);
+void				loop_shell(t_env *env);
+void				is_command(char *cmd, t_env *env);
+void				ft_echo(t_env *env);
+void				ft_echo_n(int i, int tmp, t_env *env);
+void				ft_echo_two(t_env *env);
+void				ft_not_found(char *cmd);
+void				ft_pwd(void);
+void				ft_cd(t_env *env);
+void				ft_env(t_env *env);
+void				is_pipe_here(t_env *env);
+void				ft_pipe_is_cmd(t_env *env, int old_fd);
+void				verify_cmd_pipe(char *cmd, int indic, t_env *env);
+void				ft_get_dir(t_env *env);
+void				ft_export(char *str, t_env *env);
+t_var				*ft_lstvar(t_env *env);
+void				ft_save(t_env *env, t_var *var);
+void				ft_lstaddvar2(t_var *new, t_env *env);
+void				ft_lstaddvar(t_var **alst, t_var *new);
+t_var				*ft_lstnewvar(char *var, int i, t_env *env);
+int					ft_find_char(char *str, char c);
+int					ft_verif_var(char *name, t_env *env, char *value);
+void				ft_clear(void);
+void				ft_unset(char *str, t_env *env);
+void				ft_unset_var(char *str, t_env *env);
+char				*ft_get_home(t_env *env);
+int					is_executable(t_env *env, int indic);
+char				**split_commands(t_env *env);
+int					is_multi_line_quote(t_cmd *cmd, int i);
+void				is_multi_line_quote_two(t_cmd *cmd, int i);
+char				*get_semi_coma(t_cmd *cmd, char *str, char c);
+void				ft_use_env_var(t_env *env);
 
 #endif
