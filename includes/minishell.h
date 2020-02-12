951fa2b27287d50f88d26ffb916c474eb4c5bbcc
Antoine LOUBIERES <anloubie@e1r12p20.42.fr>
2020-02-12 16:45:00 +0100

    norme .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
+/*   By: anloubie <anloubie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 18:49:54 by thverney          #+#    #+#             */
-/*   Updated: 2020/02/11 14:41:18 by antoine          ###   ########.fr       */
+/*   Updated: 2020/02/12 16:37:38 by anloubie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,76 +75,76 @@ typedef struct		s_env
 /*
 **	Main and redirections
 */
-void			loop_shell(t_env *env);
-void			is_command(char *cmd, t_env *env);
-void			is_pipe_here(t_env *env);
-void			ft_pipe_is_cmd(t_env *env, int old_fd);
-void			verify_cmd_pipe(char *cmd, int indic, t_env *env);
-char			**split_commands(t_env *env);
-int				is_multi_line_quote(t_cmd *cmd, int i);
-void			is_multi_line_quote_two(t_cmd *cmd, int i);
-char			*get_semi_coma(t_cmd *cmd, char *str, char c);
-int				how_many_backslash(char *str, int i, t_cmd *cmd);
-char			*get_semi_coma(t_cmd *cmd, char *str, char c);
-char			**split_pipes(t_env *env);
+void				loop_shell(t_env *env);
+void				is_command(char *cmd, t_env *env);
+void				is_pipe_here(t_env *env);
+void				ft_pipe_is_cmd(t_env *env, int old_fd);
+void				verify_cmd_pipe(char *cmd, int indic, t_env *env);
+char				**split_commands(t_env *env);
+int					is_multi_line_quote(t_cmd *cmd, int i);
+void				is_multi_line_quote_two(t_cmd *cmd, int i);
+char				*get_semi_coma(t_cmd *cmd, char *str, char c);
+int					how_many_backslash(char *str, int i, t_cmd *cmd);
+char				*get_semi_coma(t_cmd *cmd, char *str, char c);
+char				**split_pipes(t_env *env);
 
 /*
 **	Echo
 */
-void			ft_echo(t_env *env);
-void			ft_echo_n(int i, int tmp, t_env *env);
-void			ft_echo_two(t_env *env);
+void				ft_echo(t_env *env);
+void				ft_echo_n(int i, int tmp, t_env *env);
+void				ft_echo_two(t_env *env);
 
 /*
 **	Cd
 */
-void			ft_cd(t_env *env);
+void				ft_cd(t_env *env);
 
 /*
 **	Pwd
 */
-void			ft_pwd(void);
-void			ft_get_dir(t_env *env);
+void				ft_pwd(void);
+void				ft_get_dir(t_env *env);
 
 /*
 **	Env
 */
-void			ft_env(t_env *env);
-void			ft_save(t_env *env, t_var *var);
-void			ft_lstaddvar2(t_var *new, t_env *env);
-void			ft_lstaddvar(t_var **alst, t_var *new);
-t_var			*ft_lstnewvar(char *var, int i, t_env *env);
-int				ft_find_char(char *str, char c);
-int				ft_verif_var(char *name, t_env *env, char *value);
-t_var			*ft_lstvar(t_env *env);
+void				ft_env(t_env *env);
+void				ft_save(t_env *env, t_var *var);
+void				ft_lstaddvar2(t_var *new, t_env *env);
+void				ft_lstaddvar(t_var **alst, t_var *new);
+t_var				*ft_lstnewvar(char *var, int i, t_env *env);
+int					ft_find_char(char *str, char c);
+int					ft_verif_var(char *name, t_env *env, char *value);
+t_var				*ft_lstvar(t_env *env);
 
 /*
 **	Export
 */
-void			ft_export(t_env *env);
+void				ft_export(t_env *env);
 
 /*
 **	Unset
 */
-void			ft_unset(t_env *env);
-void			ft_unset_var(char *str, t_env *env);
+void				ft_unset(t_env *env);
+void				ft_unset_var(char *str, t_env *env);
 
 /*
 **	$$
 */
-char			*ft_get_home(t_env *env);
-void			ft_use_env_var(t_env *env);
+char				*ft_get_home(t_env *env);
+void				ft_use_env_var(t_env *env);
 
 /*
 **	Not found
 */
-void			ft_not_found(char *cmd);
+void				ft_not_found(char *cmd);
 
 /*
 **	Clear screen
 */
-void			ft_clear_screen(void);
+void				ft_clear_screen(void);
 
-int				is_executable(t_env *env, int indic);
-void			is_word(t_cmd *cmd, int i);
+int					is_executable(t_env *env, int indic);
+void				is_word(t_cmd *cmd, int i);
 #endif
