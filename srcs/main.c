a72ac9716daba5cb936b44127dbffecd31202d12
Antoine LOUBIERES <anloubie@e1r10p23.42.fr>
2020-02-17 18:20:21 +0100

    rien n'as avance yes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: anloubie <anloubie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 16:54:21 by anloubie          #+#    #+#             */
-/*   Updated: 2020/02/17 18:11:28 by anloubie         ###   ########.fr       */
+/*   Updated: 2020/02/17 18:18:59 by anloubie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,10 +74,9 @@ void	prompt_display(t_env *env)
 	loop_shell(env);
 }
 
-void	get_signal()
+void	get_signal(void)
 {
 	signal(SIGINT, ft_sig_kill);
-	signal(SIGQUIT, ft_sig_quit);
 }
 
 int		main(int ac, char **av, char **envi)
@@ -92,9 +91,9 @@ int		main(int ac, char **av, char **envi)
 	env->my_env = envi;
 	env->var = ft_lstvar(env);
 	env->fd_red = 1;
-	get_signal();
 	while (1)
 	{
+		get_signal();
 		prompt_display(env);
 		if (env->exit != 0)
 			break ;
